61ee22f83873795b2b8f26ae662f1a33377acef7
Abou Daouda <50052665+Sall99@users.noreply.github.com>
2022-10-18 15:04:35 +0000

    feat(components ): add ingredientbuilder

--- src/pages/public/home.tsx
+++ src/pages/public/home.tsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+import { Builder } from "components";
+import { IngredientTypes } from "@types";
+
 export const Home = () => {
-  return <div>Home</div>;
+  const [ingredients] = useState<IngredientTypes>({
+    salad: 0,
+    meat: 0,
+    cheese: 0,
+    bacon: 0,
+  });
+  return (
+    <>
+      <section className="flex justify-center">
+        <Builder ingredients={ingredients} />
+      </section>
+    </>
+  );
 };
