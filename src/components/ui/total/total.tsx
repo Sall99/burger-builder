8afdbd58004815365c17aa46d66afdbae8eaa93f
Abou Daouda <50052665+Sall99@users.noreply.github.com>
2022-10-21 10:00:39 +0000

    feat(styles): add responsive on total price and breads

--- src/components/ui/total/total.tsx
+++ src/components/ui/total/total.tsx
@@ -1,8 +1,9 @@
+import { BiDollar } from "react-icons/bi";
 import { Button } from "../button";
 
 export const Total = () => {
   return (
-    <div className="total-order">
+    <div className="total-order absolute left-16 top-0 hidden md:block">
       <table>
         <thead>
           <tr>
@@ -29,7 +30,9 @@ export const Total = () => {
           </tr>
           <tr>
             <td className="price"></td>
-            <td className="price">$</td>
+            <td className="price">
+              <BiDollar />
+            </td>
           </tr>
           <tr>
             <td>
