ca625b5eaa01f1686760645c3d9b267d1295ff91
Abou Daouda <50052665+Sall99@users.noreply.github.com>
2022-10-22 13:12:30 +0000

    feat(components): display ingredients

--- src/pages/public/home.tsx
+++ src/pages/public/home.tsx
@@ -1,14 +1,12 @@
-import { useState } from "react";
+import { StateProps } from "@types";
 import { Builder, Controls, Total, TotalMobile } from "components";
-import { IngredientTypes } from "@types";
+import { useSelector } from "react-redux";
 
 export const Home = () => {
-  const [ingredients] = useState<IngredientTypes>({
-    salad: 0,
-    meat: 0,
-    cheese: 0,
-    bacon: 0,
-  });
+  const { ingredients } = useSelector(
+    (state: StateProps | any) => state.ingredients
+  );
+
   return (
     <>
       <section className="flex flex-col items-center px-8 sm:px-16 justify-center relative">
