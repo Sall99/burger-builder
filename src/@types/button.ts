9014866dada85a275c1672ea4019900fb90b324f
Abou Daouda <50052665+Sall99@users.noreply.github.com>
2022-10-26 15:18:46 +0000

    feat(auth): ad yup

--- src/@types/button.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-export interface ButtonProps {
-  label: string;
-  className?: string;
-  disabled?: boolean;
-  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  type?: "button" | "submit" | "reset";
-}
